f14a9d3956c71a4d8910d99954badfe6c292b3df
Anderson Salles <agsalles@gmail.com>
2018-11-03 19:32:20 -0300

    13.2. Adicionando seletor de data (componente Calendar)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import {InputTextModule} from 'primeng/inputtext';
@@ -6,24 +7,32 @@ import {ButtonModule} from 'primeng/button';
 import {TableModule} from 'primeng/table';
 import { AppComponent } from './app.component';
 import {TooltipModule} from 'primeng/tooltip';
+import {InputTextareaModule} from 'primeng/inputtextarea';
 import { AppRoutingModule } from './app-routing.module';
+import {CalendarModule} from 'primeng/calendar';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LancamentosPesquisaComponent,
     NavbarComponent,
-    PessoasPesquisaComponent
+    PessoasPesquisaComponent,
+    LancamentoCadastroComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     InputTextModule,
     ButtonModule,
     TableModule,
     TooltipModule,
+    InputTextareaModule,
+    CalendarModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
